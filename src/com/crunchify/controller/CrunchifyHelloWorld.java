128ef7a2b1c6f4633713e38ca7711381cf4c9a7f
lsinha <lsinha@DFWLW75GT45H2.infosolco.net>
2018-12-07 14:39:31 -0600

    second commit

--- src/com/crunchify/controller/CrunchifyHelloWorld.java
+++ src/com/crunchify/controller/CrunchifyHelloWorld.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
  
 @Controller
 public class CrunchifyHelloWorld {
- 
+ // Second commit
 	@RequestMapping("/welcome")
 	public ModelAndView helloWorld() {
  
